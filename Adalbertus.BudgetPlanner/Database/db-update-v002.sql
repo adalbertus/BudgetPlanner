40a1ec0bc8eb414c05bb5ca09772920c81a2d157
Wojciech Pietkiewicz <wojciech@pietkiewicz.pl>
2012-01-12 13:07:42 +0100

    Bugfix

--- Adalbertus.BudgetPlanner/Database/db-update-v002.sql
+++ Adalbertus.BudgetPlanner/Database/db-update-v002.sql
@@ -1,1 +1,2 @@
-﻿
+﻿UPDATE [Configuration] SET [Value] = 'http://budzet-domowy.pietkiewicz.pl/pomoc' WHERE [Key] = 'HelpPage';
+UPDATE [Configuration] SET [Value] = '2' WHERE [Key] = 'DatabaseVersion';
